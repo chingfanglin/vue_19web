f0e66c11a1e85a5bc5567a06c68c8787ddd511dc
harrylin <linhtc12@gmail.com>
2017-05-17 22:56:45 +0800

    update report

--- src/pages/report/report.vue
+++ src/pages/report/report.vue
@@ -6,9 +6,20 @@
       <tab :line-width=2 active-color='#fc378c' v-model="index">
         <tab-item class="vux-center" :selected="demo2 === item" v-for="(item, index) in list2" @click="demo2 = item" :key="index">{{item}}</tab-item>
       </tab>
-      <swiper v-model="index" height="100px" :show-dots="false">
+      <swiper v-model="index" height="260px" :show-dots="false">
         <swiper-item v-for="(item, index) in list2" :key="index">
-          <div class="tab-swiper vux-center">{{item}} Container</div>
+          <div v-if="item==='收入'">
+            <cell :title="'2017/04/30'" is-link :border-intent="false" :arrow-direction="showContent002 ? 'up' : 'down'" @click.native="showContent002 = !showContent002"></cell>
+            <template v-if="showContent002">
+              <form-preview :header-label="'付款金额'" header-value="¥7500.00" :body-items="list" :footer-buttons="buttons1"></form-preview>
+            </template>
+          </div>
+          <div v-if="item==='逾期'">
+  
+          </div>
+          <div v-if="item==='支出'">
+  
+          </div>
         </swiper-item>
       </swiper>
     </div>
@@ -20,7 +31,7 @@
 import HeaderBar from '../../components/header.vue'
 import FooterBar from '../../components/footer.vue'
 
-import { Tab, TabItem, Swiper, SwiperItem } from 'vux'
+import { Tab, TabItem, Swiper, SwiperItem, Cell, CellBox, FormPreview } from 'vux'
 const list = () => ['收入', '逾期', '支出']
 
 export default {
@@ -30,13 +41,40 @@ export default {
     Swiper,
     SwiperItem,
     HeaderBar,
-    FooterBar
+    FooterBar,
+    Cell,
+    CellBox,
+    FormPreview
   },
   data() {
     return {
       list2: list(),
       demo2: '收入',
-      index: 0
+      index: 0,
+      showContent002: false,
+      list: [{
+        label: '項目',
+        value: '房租'
+      }, {
+        label: '來源',
+        value: '101 王曉明'
+      }, {
+        label: '金額',
+        value: '7500'
+      }],
+      buttons1: [{
+        style: 'default',
+        text: '修改',
+        onButtonClick: (name) => {
+          alert(`clicking ${name}`)
+        }
+      }, {
+        style: 'primary',
+        text: '刪除',
+        onButtonClick: (name) => {
+          alert(`clicking ${name}`)
+        }
+      }]
     }
   }
 }
